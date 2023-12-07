6061cdd3cb9c658ba6258d38e7eedb1ed7b56f94
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-12-07 18:05:16 +0530

    small fixes

--- web/main.js
+++ web/main.js
@@ -1,36 +1,43 @@
-console.log("hello")
+console.log("hello");
 
-function append_book_items(book_list,admin=false) {
-  let parent = document.querySelector(".books")
-  parent.innerHTML=""
+function append_book_items(book_list, admin = false) {
+  let parent = document.querySelector(".books");
+  parent.innerHTML = "";
   for (book of book_list) {
     let book_element = `
-    <li class="${admin ? `admin-list-items`: `list-items`}">
+    <li class="${admin ? `admin-list-items` : `list-items`}">
       <span>${book[1]}</span>
       <span>${book[2]}</span>
       <span>${book[3]}</span>
-      ${admin ? `<span>${book[4]}</span> <span> <form> <input class= "restock-${book[0]}" type = "number" required placeholder="Enter Quantity"> <button onclick="handleRestock(event, '${book[4]}', '${book[0]}')">Restock</button> </form> </span>`
-      : `<span><button class="buttons" onclick="window.location.href='/issuebooks.html?book=${book[1]}'">Issue</button></span>`}
+      ${
+        admin
+          ? `<span>${book[4]}</span> <span> <form> <input class= "restock-${book[0]}" type = "number" required placeholder="Enter Quantity"> <button onclick="handleRestock(event, '${book[4]}', '${book[0]}')">Restock</button> </form> </span>`
+          : `<span><button class="buttons" onclick="window.location.href='/issuebooks.html?book=${book[1]}'">Issue</button></span>`
+      }
     </Li>
-    `
-    parent.innerHTML += book_element
+    `;
+    parent.innerHTML += book_element;
   }
 }
 
-var url_string = window.location.href
+var url_string = window.location.href;
 var url = new URL(url_string);
 var book = url.searchParams.get("book");
-document.querySelector(".form-book").innerHTML=`Book: ${book}`
+document.querySelector(".form-book").innerHTML = `Book: ${book}`;
 
 function handleAdminAccess(event) {
   event.preventDefault();
   let password = document.querySelector(".admin-pass").value;
   console.log(password);
 
-  eel.check_pass(password)().then(function(res) {
-    res ? window.location.href = "/admin.html" : alert("WRONG PASSWORD ENTERED!");
-  })
-  
+  eel
+    .check_pass(password)()
+    .then(function (res) {
+      res
+        ? (window.location.href = "/admin.html")
+        : alert("WRONG PASSWORD ENTERED!");
+    });
+
   document.querySelector(".admin-pass").value = "";
 }
 
@@ -40,7 +47,9 @@ function handleIssueBook(event) {
   let name = document.querySelector(".username").value;
   console.log(name);
 
-  eel.issue_book(name, book)().then(res => console.log(res))
+  eel
+    .issue_book(name, book)()
+    .then((res) => console.log(res));
   alert("Book Has Been Issued. Pick it from the counter");
 }
 
@@ -49,20 +58,26 @@ function handleAddBook(event) {
   let book_name = document.querySelector(".book_name").value;
   console.log(book_name);
 
-  let genre=document.querySelector(".genre").value;
+  let genre = document.querySelector(".genre").value;
   console.log(genre);
 
-  let fine=Number(document.querySelector(".fine").value);
+  let fine = Number(document.querySelector(".fine").value);
   console.log(fine);
 
-  let quantity=Number(document.querySelector(".book_quantity").value);
+  let quantity = Number(document.querySelector(".book_quantity").value);
   console.log(quantity);
 
-  eel.add_book(book_name,genre,fine,quantity)().then(res => console.log(res))
+  eel
+    .add_book(book_name, genre, fine, quantity)()
+    .then((res) => console.log(res));
+  location.reload();
 }
 
 function handleRestock(event, quantity, bookId) {
-  event.preventDefault()
+  event.preventDefault();
   let newQuantity = Number(document.querySelector(`.restock-${bookId}`).value);
-  eel.restock(newQuantity + Number(quantity), bookId)().then(res => console.log(res))
+  eel
+    .restock(newQuantity + Number(quantity), bookId)()
+    .then((res) => console.log(res));
+  location.reload();
 }
